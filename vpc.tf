767e5b4cb40e41c521fbd3c09ca168fae8d51867
Williams Barriquero <williamsgomess@outlook.com>
2024-05-21 16:56:58 -0300

    feat: Add two new subnets to VPC configuration
    The code changes introduce two new subnets to the VPC configuration. This enhances network segmentation and resource isolation within the VPC.

--- vpc.tf
+++ vpc.tf
@@ -1,26 +1,28 @@
 resource "aws_vpc" "new-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "maestro-vpc"
+    Name = "${var.prefix}-vpc"
   }
 }
 
 data "aws_availability_zones" "available" {}
 
-resource "aws_subnet" "new-subnet-1" {
-  availability_zone = data.aws_availability_zones.available.names[0]
-  vpc_id            = aws_vpc.new-vpc.id
-  cidr_block        = "10.0.0.0/24"
+resource "aws_subnet" "subnets" {
+  count                   = 2
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  vpc_id                  = aws_vpc.new-vpc.id
+  cidr_block              = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
   tags = {
-    Name = "maestro-subnet-1"
+    Name = "${var.prefix}-subnet-${count.index + 1}"
   }
 }
 
-resource "aws_subnet" "new-subnet-2" {
-  availability_zone = data.aws_availability_zones.available.names[1]
-  vpc_id            = aws_vpc.new-vpc.id
-  cidr_block        = "10.0.1.0/24"
-  tags = {
-    Name = "maestro-subnet-2"
-  }
-}
+# resource "aws_subnet" "new-subnet-1" {
+#   availability_zone = data.aws_availability_zones.available.names[0]
+#   vpc_id            = aws_vpc.new-vpc.id
+#   cidr_block        = "10.0.0.0/24"
+#   tags = {
+#     Name = "${var.prefix}-subnet-${count.index + 1}"
+#   }
+# }
