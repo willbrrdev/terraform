93ea3e2f0958ecdd7edfb3a04d325be1215a5752
Williams Barriquero <williamsgomess@outlook.com>
2024-05-21 17:22:01 -0300

    feat: Add AWS internet gateway and route table to VPC configuration
    The code changes introduce an AWS internet gateway and route table to the VPC configuration. This enables internet connectivity for the VPC and allows traffic routing to the internet.

--- vpc.tf
+++ vpc.tf
@@ -18,6 +18,30 @@ resource "aws_subnet" "subnets" {
   }
 }
 
+resource "aws_internet_gateway" "new-igw" {
+  vpc_id = aws_vpc.new-vpc.id
+  tags = {
+    Name = "${var.prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "new-rtb" {
+  vpc_id = aws_vpc.new-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.new-igw.id
+  }
+  tags = {
+    Name = "${var.prefix}-rtb"
+  }
+}
+
+resource "aws_route_table_association" "new-rtb-association" {
+  count          = 2
+  route_table_id = aws_route_table.new-rtb.id
+  subnet_id      = aws_subnet.subnets.*.id[count.index]
+}
+
 # resource "aws_subnet" "new-subnet-1" {
 #   availability_zone = data.aws_availability_zones.available.names[0]
 #   vpc_id            = aws_vpc.new-vpc.id
