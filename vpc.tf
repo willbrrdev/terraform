352c391df0db23c96d12f7ed19f4c9296c3229da
Williams Barriquero <williamsgomess@outlook.com>
2024-05-21 16:30:18 -0300

    feat: Add new subnets to VPC configuration
    The code changes add two new subnets to the VPC configuration. This allows for better network segmentation and resource isolation within the VPC.

--- vpc.tf
+++ vpc.tf
@@ -4,3 +4,23 @@ resource "aws_vpc" "new-vpc" {
     Name = "maestro-vpc"
   }
 }
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "new-subnet-1" {
+  availability_zone = data.aws_availability_zones.available.names[0]
+  vpc_id            = aws_vpc.new-vpc.id
+  cidr_block        = "10.0.0.0/24"
+  tags = {
+    Name = "maestro-subnet-1"
+  }
+}
+
+resource "aws_subnet" "new-subnet-2" {
+  availability_zone = data.aws_availability_zones.available.names[1]
+  vpc_id            = aws_vpc.new-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  tags = {
+    Name = "maestro-subnet-2"
+  }
+}
