b3e1ae95b6890f267ab537b8c82a46fa47dc3c90
Williams Barriquero <willbrr.dev@gmail.com>
2024-07-08 20:46:53 -0300

    Iniciando módulo do EKS

---
+++ modules/eks/variables.tf
@@ -0,0 +1,8 @@
+variable "vpc_id" {}
+variable "prefix" {}
+variable "cluster_name" {}
+variable "rentention_days" {}
+variable "subnet_ids" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
