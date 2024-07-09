cb241045a12e694e8c345a9d27e3dd6c03021cf8
Williams Barriquero <willbrr.dev@gmail.com>
2024-07-08 22:59:06 -0300

    Finalizando módulos

---
+++ main.tf
@@ -0,0 +1,17 @@
+module "new-vpc" {
+  source         = "./modules/vpc"
+  prefix         = var.prefix
+  vpc_cidr_block = var.vpc_cidr_block
+}
+
+module "eks" {
+  source           = "./modules/eks"
+  cluster_name     = var.cluster_name
+  rentention_days  = var.rentention_days
+  desired_capacity = var.desired_capacity
+  max_size         = var.max_size
+  min_size         = var.min_size
+  vpc_id           = module.new-vpc.vpc_id
+  subnet_ids       = module.new-vpc.subnet_ids
+  prefix           = var.prefix
+}
--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable "prefix" {}
+variable "vpc_cidr_block" {}
 variable "cluster_name" {}
 variable "rentention_days" {}
 variable "desired_capacity" {}
